2fe56d6bf6d3ad78c70ad7a48c75cf32a1b326d6
samrat <98076413+samrtamandal100@users.noreply.github.com>
2022-08-12 11:48:03 +0530

    Add files via upload

---
+++ Map/Longest_Palindrome.java
@@ -0,0 +1,35 @@
+ class Solution {
+    public int longestPalindrome(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        
+        for(int i=0;i<s.length();i++)
+        {
+            char c =s.charAt(i);
+            if(map.containsKey(c))
+                map.put(c ,map.get(c)+1);
+            else
+                map.put(c ,1);
+        }
+        
+        int ans=0;
+        boolean firstodd =false;
+        
+        for(Character key :map.keySet())
+        {
+            if(map.get(key)%2==0)
+            {
+                ans+=map.get(key);
+            }else
+            {
+                if(firstodd ==false)
+                {
+                    ans+=map.get(key);
+                    firstodd =true;
+                }else
+                    ans+=map.get(key)-1;
+            }
+        }
+        return ans;
+        
+    }
+}
